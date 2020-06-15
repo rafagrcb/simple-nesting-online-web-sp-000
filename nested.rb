a6bbb4545102b0dbd70824d26d18155a9acedd1d
Rafael Garcia <rafagrcb@gmail.com>
2020-06-15 15:16:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto] = {know_for: "Ruby", languages: ["LISP", "C"]  
+  programmer_hash[:yukihiro_matsumoto] = {know_for: "Ruby", languages: ["LISP", "C"]}  
 end
 
 def changing_alan
